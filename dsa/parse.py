5fcff66373c80eb632e54fa2ff4b29856d9b949a
Dukunde Irakoze Happy <acele4444@gmail.com>
2025-09-30 19:58:40 +0200

    Create dsa/parse.py

---
+++ dsa/parse.py
@@ -0,0 +1,37 @@
+import xml.etree.ElementTree as ET
+from typing import Any
+
+def get_messages(data):
+    """This function gets all messages"""
+    all_data = data.findall("sms")
+    if not all_data:
+        return []
+    return [node.attrib["body"] for node in all_data]
+
+class TransactionMessages:
+    def __init__(self, messages: Any):
+        self.__all_messages = messages
+
+    def check_messages(self, messages) -> list[str]:
+        """This function checks if message is available"""
+        if not messages:
+            return None
+        self.__all_messages = messages
+        return self.__all_messages
+
+    def get_momo_income(self, messages) -> list[str]:
+        """This function gets all income messages from Momo to Momo"""
+        if not self.check_messages(messages):
+            return []
+        return [
+            message for message in self.__all_messages
+            if message.startswith("You have received")
+        ]
+
+if __name__ == "__main__":
+    momo_data = ET.parse(r"E:\MoMo_Data_Hub\data\modified_sms_v2.xml")
+    all_messages = get_messages(data=momo_data.getroot())
+    user_transaction = TransactionMessages(messages=all_messages)
+
+    for pay in user_transaction.get_momo_income(messages=all_messages):
+        print(f"{pay}\n")
